ca19f423ff859e43510b4df3cd29f1cffbb85e4e
sampath9dasari <sampath9dasari@gmail.com>
2020-03-30 23:13:02 -0400

    fix dynamic batches function

--- lib/perceptron.py
+++ lib/perceptron.py
@@ -1,22 +1,26 @@
+import numpy as np
 from lib.perceptron_helper import *
 
+class Perceptron:
 
-class Perceptron():
-
-    def __init__(self, epoch_threshold=100, l_rate=1, batch_size=1):
-        self.weights = np.random.uniform(size=4)
+    def __init__(self, dim=1, epoch_threshold=100, l_rate=1, batch_size=1):
+        self.dim = dim
+        self.weights = np.random.uniform(size=dim + 1)
         self.epoch_threshold = epoch_threshold
         self.l_rate = l_rate
         self.batch_size = batch_size
         pass
 
-    ###
-    def compute(self, x, y, weights):
+
+    def compute(self, x, weights):
         """
         Fully vectorized function to compute the product between weights, feature vector and true label
         """
 
-        y_compute = np.dot(x, weights) * y
+        if weights is None:
+            weights = self.weights
+
+        y_compute = np.dot(x, weights)
         return y_compute
 
     def fit(self, train, labels, init_weights=None, l_rate=None, batch_size=None, epoch_threshold=None, verbose=False):
@@ -25,20 +29,22 @@ class Perceptron():
 
         :param train: Array of input to train the model on
         :param labels: Array of output labels for loss computation
-        :param init_weights:
-        :param l_rate:
-        :param batch_size:
-        :param epoch_threshold:
-        :param verbose:
-        :return:
+        :param init_weights: a
+        :param l_rate: b
+        :param batch_size: c
+        :param epoch_threshold: d
+        :param verbose: e
+        :return: f
         """
 
         epoch = 0
 
-        if init_weights is None : weights = self.weights
-        else : weights = init_weights
-        if l_rate is None : l_rate = self.l_rate
-        if batch_size is None : batch_size = self.batch_size
+        if init_weights is None:
+            weights = self.weights
+        else:
+            weights = init_weights
+        if l_rate is None: l_rate = self.l_rate
+        if batch_size is None: batch_size = self.batch_size
         if epoch_threshold is None: epoch_threshold = self.epoch_threshold
 
         train_batches = make_batches(train, batch_size, 0)
@@ -50,7 +56,7 @@ class Perceptron():
         while epoch <= epoch_threshold:
             total_miss_class = 0
             for tbatch, lbatch, batch_num in zip(train_batches, label_batches, range(len(train_batches) + 1)):
-                computed_value = self.compute(tbatch, lbatch, weights)
+                computed_value = self.compute(tbatch, weights) * lbatch
                 miss_class = len(tbatch[computed_value < 0])
                 total_miss_class += miss_class
 
@@ -65,11 +71,21 @@ class Perceptron():
 
             epoch += 1
 
+        self.weights = weights
         print()
         print("\nClassification Hyperplane : %.2fx + %.2fy + %.2fz + %.2f" % tuple(weights))
 
         return weights, epoch + 1
 
-
     def predict(self, test):
+        """
+        Predict the labels using the learned weights from the fit function.
 
+        :param test: Array of input data to make the predictions on
+        :return: Array of output labels on the input data
+        """
+
+        labels = self.compute(test)
+        pred_labels = np.where(labels < 0, -1, 1)
+
+        return pred_labels
--- lib/perceptron_helper.py
+++ lib/perceptron_helper.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def make_batches(data, axis, batch_size):
+def make_batches(data, batch_size, axis=0):
     """
     Function to make batches, given an numpy array, batch size and the axis for splitting
 
@@ -10,10 +10,23 @@ def make_batches(data, axis, batch_size):
     :param batch_size: An integer value giving the size of each batch
     :return: An array containing arrays of input data, each array being a different batch
     """
+
     num_batches = data.shape[axis] // batch_size
-    residue = data.shape[0] % batch_size
+    residue = data.shape[axis] % batch_size
     batches = np.split(data[:data.shape[axis] - residue], num_batches)
     if residue != 0:
         batches = batches + [data[data.shape[axis] - residue:]]
 
     return batches
+
+
+def accuracy(pred_labels, actual_labels):
+    """
+    Calculates the accuracy between the predicted label and actual labels.
+
+    :param pred_labels: Array of predicted output labels of data set.
+    :param actual_labels: Array of actual output labels of data set.
+    :return: A float value giving the accuracy.
+    """
+
+    return np.array([pred_labels == actual_labels]).mean()
